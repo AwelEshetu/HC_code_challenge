196ed7f00259b0484aff9787799953b13fe1dce3
Awel <aweloe@gmail.com>
2020-12-17 13:37:53 +0200

    some cleanup

--- src/index.js
+++ src/index.js
@@ -1,24 +1,20 @@
 const express = require("express"),
-      stringFormatter = require('./codeChallenge').stringFormatter
-const app = express(),
-      bodyParser = require('body-parser');
+      stringFormatter = require('./codeChallenge').stringFormatter;
+const app = express();
 
-app.use(bodyParser.json())
-
+//app.use(bodyParser.json())
 app.use((req, res, next) => {
     req['rawBody'] = '';
     req.on('data', (chunk) => (req['rawBody'] += chunk));
     req.on('end', () => {
-        req.body = req['rawBody'];
+        req.body = JSON.parse(req['rawBody']);
         next();
     });
 });
-app.post("/sananmuunnos/",  (req, res) => {
+
+app.post("/",  (req, res) => {
   const body = req.body;
-  console.log('body is ',body)
-  console.log('type of body is ', typeof(body))
-  
-  res.send('hello world')
+  res.send(JSON.stringify(stringFormatter(body)))
 });
 
 const PORT = 3002;
